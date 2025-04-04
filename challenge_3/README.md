31c244fc3dd8af4458c655060e9c5d9bff18357a
elusivenode <elusivenode@gmail.com>
2025-04-04 14:40:51 +1000

    challenge 3 description

---
+++ challenge_3/README.md
@@ -0,0 +1,19 @@
+# 📚 Library Book Tracker
+
+This is challenge will be around mananging books in a library. It uses a list of dictionaries to store information about each book, including:
+
+- The book's **title**
+- The **author**
+- Whether the book is currently **borrowed**
+
+## 🚀 Write functions to do the following
+
+- View a list of all books and their status
+- Borrow a book (if it's available)
+- Return a book
+- Add new books to the library
+
+## 🏃‍♂️ How to Run
+
+```zsh
+python3 library_tracker.py
---
+++ challenge_3/challenge_3.py
@@ -0,0 +1,24 @@
+library = [
+    {"title": "Harry Potter", "author": "J.K. Rowling", "borrowed": False},
+    {"title": "The Hobbit", "author": "J.R.R. Tolkien", "borrowed": False},
+    {"title": "1984", "author": "George Orwell", "borrowed": True},
+    {"title": "To Kill a Mockingbird", "author": "Harper Lee", "borrowed": False},
+    {"title": "The Great Gatsby", "author": "F. Scott Fitzgerald", "borrowed": True},
+    {"title": "Moby Dick", "author": "Herman Melville", "borrowed": False},
+    {"title": "Pride and Prejudice", "author": "Jane Austen", "borrowed": True},
+    {"title": "War and Peace", "author": "Leo Tolstoy", "borrowed": False},
+    {"title": "The Catcher in the Rye", "author": "J.D. Salinger", "borrowed": False},
+    {"title": "Brave New World", "author": "Aldous Huxley", "borrowed": True},
+    {"title": "The Lord of the Rings", "author": "J.R.R. Tolkien", "borrowed": False},
+    {"title": "The Chronicles of Narnia", "author": "C.S. Lewis", "borrowed": True},
+    {"title": "Charlotte's Web", "author": "E.B. White", "borrowed": False},
+    {"title": "Frankenstein", "author": "Mary Shelley", "borrowed": True},
+    {"title": "The Odyssey", "author": "Homer", "borrowed": False},
+    {"title": "Crime and Punishment", "author": "Fyodor Dostoevsky", "borrowed": True},
+    {"title": "Jane Eyre", "author": "Charlotte Brontë", "borrowed": False},
+    {"title": "Little Women", "author": "Louisa May Alcott", "borrowed": False},
+    {"title": "Animal Farm", "author": "George Orwell", "borrowed": True},
+    {"title": "The Alchemist", "author": "Paulo Coelho", "borrowed": False},
+]
+
+print(library)
